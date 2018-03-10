7cd029b1fb91a088a0e2d1dc712fea700b94c752
kirFish <32800952+kirFish@users.noreply.github.com>
2018-03-10 13:50:07 +0200

    Add files via upload

---
+++ RandomWordParser.go
@@ -0,0 +1,132 @@
+package main
+
+import(
+
+	_"github.com/andybalholm/cascadia"
+	"fmt"
+	"net/http"
+	"strconv"
+	"io/ioutil"
+	"log"
+	"strings"
+	"time"
+	"math/rand"
+	"github.com/PuerkitoBio/goquery"
+	_"go/doc"
+)
+
+type RowWordData struct{
+	Word string
+	Definition []string
+	UsageExample []string
+	WordRating int
+}
+
+func main() {
+
+	// making random int from 0 : 88800
+	var example RowWordData
+	rand.Seed(time.Now().UnixNano())
+	index:= rand.Intn(88800)
+	// initializing the "WordRating" field in example of RowWordData
+	example.WordRating = index
+	// trying to get value of "Word" to type it into "Word" field in example of RowWordData
+	word := wordCountWorker(example)
+	example.Word = word
+	// Getting 5 usage examples for
+	usageExamples := dictionaryUsageExample(example)
+	example.UsageExample = usageExamples
+	// gettting 5 or less definitions
+	dictionaryDefinitionExample(example)
+
+}
+// got example like an argument and return "word" type - string
+func  wordCountWorker(example RowWordData)(string) {
+
+	// got an example like an argument and parsing word from site with "WordRating" rating
+	client := http.Client{}
+	url := "http://www.wordcount.org/dbquery.php?toFind="
+	url = url + strconv.Itoa(example.WordRating-1)
+	url = url + "&method=SEARCH_BY_INDEX"
+
+	resp, err := client.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	wholeLine := string(body)
+	//got a response from site
+	word := wholeLine[strings.Index(wholeLine, "word0")+6:strings.Index(wholeLine, "&freq0")]
+	//got a word from full response
+	fmt.Println(word)
+	//return word
+	return word
+}
+func  dictionaryUsageExample(example RowWordData)([]string){
+	urlToParse := "https://en.oxforddictionaries.com/definition/"
+	//need to change the word to example.Word
+	urlToParse = urlToParse + "chaos"
+
+	doc2, err := goquery.NewDocument(urlToParse)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	usageExamples := make([]string, 0)
+	doc2.Find(".examples .exg .ex").Each(func(i int, s *goquery.Selection) {
+
+		usageExample := s.Find("em").Text()
+		if(i<5) {
+			usageExamples = append(usageExamples , usageExample)
+		}
+
+	})
+
+	i:= 0
+	example.UsageExample = make([]string, 5)
+	for i < len(usageExamples) {
+		value := usageExamples[i]
+		example.UsageExample[i] = value
+		i++
+	}
+	return example.UsageExample
+}
+func  dictionaryDefinitionExample(example RowWordData)([]string){
+
+	urlToParse := "https://en.oxforddictionaries.com/definition/"
+	urlToParse = urlToParse + "chaos"
+
+	doc2, err := goquery.NewDocument(urlToParse)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	definitionExamples := make([]string, 0)
+	doc2.Find(".subSenses li .ind ").Each(func(i int, s *goquery.Selection) {
+
+		definitionExample := s.Find("span").Text()
+		if i<5 {
+			definitionExamples = append(definitionExamples , definitionExample)
+			fmt.Println(definitionExamples[i])
+		}
+
+	})
+
+	i:= 0
+	example.Definition = make([]string, 5)
+	for i<len(definitionExamples) {
+		value := definitionExamples[i]
+		example.UsageExample[i] = value
+		i++
+	}
+
+	return example.Definition
+}
+
