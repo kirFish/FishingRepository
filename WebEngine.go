e4fb05cb35137868d04d973ede1988bb581822b4
kirFish <32800952+kirFish@users.noreply.github.com>
2018-03-10 18:19:56 +0200

    Add files via upload

--- WebEngine.go
+++ WebEngine.go
@@ -12,9 +12,10 @@ import (
 
 type RowWordData struct {
 	Word          string
-	Definitions   []string
-	UsageExamples []string
+	Definitions   [][]string
+	UsageExamples [][]string
 	WordRang      int
+	partOfLanguage []string
 }
 
 
@@ -36,7 +37,7 @@ func GetRandomWord() (*RowWordData, error) {
 		return nil, err
 	}
 
-	wordData.Definitions, wordData.UsageExamples, err = getExtendedWordData(wordData.Word)
+	wordData.Definitions, wordData.UsageExamples, wordData.partOfLanguage ,err = getExtendedWordData(wordData.Word)
 	if err != nil {
 		return nil, err
 	}
@@ -68,28 +69,54 @@ func getWordData(index int) (word string, err error)  {
 }
 
 
-func getExtendedWordData(word string) (Definitions []string, UsageExamples []string, err error) {
+func getExtendedWordData(word string) (Definitions [][]string, UsageExamples [][]string,partOftheLanguage []string, err error) {
 	urlToParse := OXFORD_DICTIONARY_URL + word
 	wordPage,err := goquery.NewDocument(urlToParse)
+
 	if err!=nil {
-		return nil,nil,err
+
+		return nil,nil,nil,err
+
 	}
-	usageExamples := make([]string, 0)
-	wordPage.Find(".examples .exg .ex").Each(func(i int, s *goquery.Selection) {
-		usageExample := s.Find("em").Text()
-		if(i<5) {
-			usageExamples = append(usageExamples , usageExample)
-		}
+
+	partsOfTheLanguage := make([]string, 0)
+
+	wordPage.Find("section .gramb h3 .pos ").Each(func(i int, s *goquery.Selection) {
+
+			value := s.Find("span").Text()
+			partsOfTheLanguage = append(partsOfTheLanguage,value)
+
+
 	})
 
-	definitions := make([]string, 0)
-	wordPage.Find(".semb li .trg ol li" ).Each(func(i int, s *goquery.Selection) {
-		if i<5 {
+	usageExamples := make([][]string,len(partsOfTheLanguage) , 0)
+	definitions := make([][]string,len(partsOfTheLanguage) , 0)
+
+	wordPage.Find("section .gramb h3 .pos ").Each(func(i int, s *goquery.Selection) {
+
+		wordPage.Find(".semb li .trg .ind" ).Each(func(j int, s *goquery.Selection) {
+
 			definitionExample := s.Find("span").Text()
-			definitions = append(definitions, definitionExample)
-		}
+			definitions[i] = append(definitions[i],definitionExample)
+
+
+
+		})
+
+		wordPage.Find(".examples .exg .ex" ).Each(func(k int, s *goquery.Selection) {
+			if k < 5 {
+				usageExample := s.Find("em").Text()
+				usageExamples[i] = append(usageExamples[i], usageExample)
+			}
+		})
+
+
+
 	})
-	return definitions, usageExamples,nil
+
+
+
+	return definitions, usageExamples,partsOfTheLanguage,err
 }
 
 
